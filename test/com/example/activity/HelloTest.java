8a3b8d989506968837664cac1f385bba49c8b3e0
David Young-Chan Kay <davidykay@gmail.com>
2013-08-27 17:50:39 -0400

    had to manipulate the junit directory name to be ABOVE the android directory name in the import order. whatever.

--- test/com/example/activity/HelloTest.java
+++ test/com/example/activity/HelloTest.java
@@ -1,17 +1,22 @@
 import junit.framework.*;
 import static org.junit.Assert.assertEquals;
-
+import org.robolectric.RobolectricTestRunner;
 import com.example.activity.MyFirstActivity;
+import org.junit.runner.RunWith;
+import org.junit.Test;
 
+@RunWith(RobolectricTestRunner.class)
 public class HelloTest extends TestCase {
   protected void setUp() {
 
   }
 
+  @Test
   public void testAdd() {
     assertEquals(1, 2);
   }
 
+  @Test
   public void testActivitySquare() {
     MyFirstActivity activity = new MyFirstActivity();
     assertEquals(9, activity.square(3));
